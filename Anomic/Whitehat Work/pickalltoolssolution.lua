882936f6bda42a8a6f54d23cf8502aaaf8e2196e
BonfireDevelopment <79977742+BonfireDevelopment@users.noreply.github.com>
2021-07-21 12:06:10 -0700

    Create pickalltoolssolution.lua

---
+++ Anomic/Whitehat Work/pickalltoolssolution.lua
@@ -0,0 +1,11 @@
+--this wont stop anyone that has a functioning brain stem so ill leave the rest up to you
+
+--[[
+Here's how to fix pick all tools:
+
+1. Spawn a random item at a random position in the server
+2. Remove the E to interact option on the item, so that picking it up requires the remote to be fired manually
+  - Of course anyone can detect if it's removed, I'll leave that fix up to you
+3. Wait a random time
+4. Repeat
+]]
