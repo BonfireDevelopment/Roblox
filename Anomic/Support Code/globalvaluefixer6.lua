8c8daa1c95ce8c74ac5802364ab9d7c4374d2456
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2022-10-17 19:04:31 -0700

    Update globalvaluefixer6.lua

--- Anomic/Support Code/globalvaluefixer6.lua
+++ Anomic/Support Code/globalvaluefixer6.lua
@@ -1,14 +1,22 @@
-local anchortool = workspace.Entities:FindFirstChild("ToolModel")
 local lp = game.Players.LocalPlayer
-if not anchortool then game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("the hell", "All") end
-
-for i,v in next, workspace.Entities:GetChildren() do
+for i,v in pairs(game:GetService("Workspace").Entities:GetChildren()) do
+    if v.Name == "ToolModel" then
+        local distance = ((Vector3.new(1000.79315, -38.7140503, -132.486191))-(v.hitbox.Position)).Magnitude
+        if distance < 30 then
+            v:Destroy()
+        end
+    end
+end
+local origin = lp.Character.HumanoidRootPart.CFrame
+    for i,v in pairs (workspace.Entities:GetChildren()) do
         if lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") and v.Name == "ToolModel" and v.PrimaryPart then
             lp.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame
             wait(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue()/1000 + 0.2)
             for i=1,10 do
+                if not v or not v.PrimaryPart then break end
+                lp.Character.HumanoidRootPart.CFrame = v.PrimaryPart.CFrame
                 local args = {
-                    [1] = anchortool,
+                    [1] = v,
                     [2] = "PickUp"
                 }
                 
@@ -17,3 +25,5 @@ for i,v in next, workspace.Entities:GetChildren() do
             end
         end
     end
+    lp.Character.HumanoidRootPart.CFrame = origin
+    lp.Character:FindFirstChild("Humanoid"):ChangeState("Jumping")
