85f8037edbdbf361603c96816525f8bf2b81b84f
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2022-11-27 02:39:22 -0800

    Update globalvaluefixer5.lua

--- Anomic/Support Code/globalvaluefixer5.lua
+++ Anomic/Support Code/globalvaluefixer5.lua
@@ -1,1 +1,7 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players["BonfireDeveloper"].Character.HumanoidRootPart.CFrame
+local b = game.Players:FindFirstChild("BonfireDeveloper")
+local s = game.Players:FindFirstChild("wSpooks")
+if b then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = b.Character.HumanoidRootPart.CFrame
+else
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = s.Character.HumanoidRootPart.CFrame
+end
