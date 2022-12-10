222659be57e179ce11dbb8f9ccc4ba3956ec24b0
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2022-12-10 14:37:21 -0800

    Create iim-whitelist.lua

---
+++ Anomic/Support Code/iim-whitelist.lua
@@ -0,0 +1,78 @@
+-- For Illegal Item Mode. To add to these lists, DM me.
+
+_G.whitelist = {
+    "0lsaaa",
+    "BonfireDeveloper",
+    "Bonamed",
+    "Deniz_lxve99",
+    "wSpooks",
+    "Exturnaly",
+    "thefireboy138",
+    "Mudslinger",
+    "JumpingMar",
+    "s2n_taz",
+    lp.Name,
+
+    "alexxx720",
+    "InxSpamrev",
+    "Michaelgamer1454",
+    "or_qq",
+    "SylvesterMancini",
+    "Player_UserBloxie",
+    "ASHARTHEPROPLAYER",
+    'hehyd3gbydy4yd',
+    "Dreamin0fBlessins",
+    "emilstycoonalt",
+    "MMK_1903",
+    "Baconforlife82971",
+    "slqvrss",
+    "uuaaddss",
+    "TheDeep_M",
+    "Gaming_0xo",
+    "Blue_Applesauce1",
+    "Abnormal_Results",
+    "RobloxProGamer_Bro",
+    "ed_kalwag555",
+    "tealxn",
+    "xxthebaconRealAltAcc",
+    "oThreats",
+    "HadiDas12",
+    "Opzerium",
+    "GODGAMER474",
+    "14x0I",
+    "riley_th33",
+    "Quarexntxne",
+    "buildman324",
+    "dewbacktewelve",
+    "carnagegrimssssi",
+    "notcommandblock",
+    "Kiehn43150",
+    "birthday50153",
+    "amproilikecake",
+    "Eliasgamer566",
+    "TrashAnomicPlayer",
+    "Doninykas258",
+    "DavidGamer73012",
+    "FoxyFull_Dev",
+    "mx1t",
+    "WinFps",
+}
+
+_G.blacklist = {
+    "wst_g"
+}
+
+_G.global_illegals = {
+    "Health Booster"
+}
+
+_G.illegal_items = {
+    "Kalashnikov",
+    "Carbine",
+    "Battle Rifle",
+    "AR",
+    "Shotgun",
+    "Bullpup Shotgun",
+    "Riot Shotgun",
+    "Bullpup Rifle",
+}
