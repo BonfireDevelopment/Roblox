8bbc87b013ab39e2b56aba6837d546b361349a63
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2023-07-10 23:25:20 -0700

    Update tgwl.lua

--- Anomic/Support Code/tgwl.lua
+++ Anomic/Support Code/tgwl.lua
@@ -1,1 +1,1 @@
-BonfireDeveloper,1xAceFoxy,REDPOTATO40,windy_al,UltraCoolGuy986,SnowModd,DrToxz,p_oggus,traplocals,BogdanGiovanni,Thehajvb,logo00702,yay951862,burtpenguinnn
+BonfireDeveloper,1xAceFoxy,REDPOTATO40,windy_al,UltraCoolGuy986,SnowModd,DrToxz,p_oggus,traplocals,BogdanGiovanni,Thehajvb,logo00702,yay951862,burtpenguinnn,ChillyPikachu2
